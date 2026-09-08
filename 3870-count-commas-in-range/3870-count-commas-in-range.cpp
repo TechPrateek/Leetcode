95b568ee7fe4b1fd2ff3cbda0ae07209a7c04942
Prateek Yadav <prateekyadav8006@gmail.com>
2026-09-08 21:52:59 +0530

    Time: 1 ms (16.95%), Space: 9.3 MB (5.52%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int copy=n;
+        string s = to_string(copy);
+        int m = s.size();
+        int sub = pow(10,m-2);
+        if(m<4) return 0;
+        return n-999;
+    }
+};
